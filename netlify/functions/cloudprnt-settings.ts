8df541bd8c424fff5bb163f5c29b5b461729386e
Chase Kellis <chase@cardchase.org>
2026-04-09 16:42:39 -0700

    Add CloudPRNT settings handshake endpoint

---
+++ netlify/functions/cloudprnt-settings.ts
@@ -0,0 +1,18 @@
+import type { Handler } from '@netlify/functions'
+
+const handler: Handler = async () => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify({
+      title: 'star_cloudprnt_server_setting',
+      version: '1.0.0',
+      serverSupportProtocol: ['HTTP'],
+    }),
+  }
+}
+
+export { handler }
